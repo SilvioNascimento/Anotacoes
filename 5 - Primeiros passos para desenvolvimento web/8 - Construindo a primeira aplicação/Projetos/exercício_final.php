ecbcf4d793fa9535d70b9d6bd8032b470d121ea0
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2022-02-21 11:55:59 -0300

    Adicionar anotações

---
+++ 5 - Primeiros passos para desenvolvimento web/8 - Construindo a primeira aplicação/Projetos/exercício_final.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usando uma biblioteca chamada Accordion</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+    <style type='text/css'>
+        h3 {
+            font-weight: bold;
+            text-align: center;
+        }
+        .linha {
+            font-weight: lighter;
+            color: blue;
+            text-align: justify;
+        }
+    
+    </style>
+    <script>
+    $( function() {
+        $( "#accordion" ).accordion();
+        } );
+  </script>
+</head>
+
+<body>
+    <div id="accordion">
+    <h3>Apresentação</h3>
+    <div>
+        <p class="linha">
+        Olá pessoal. Tudo bem com vocês? Me chamo Silvio e este site tem como intuito de concluir o exercício final da última aula de <b>Primeiros passos para desenvolvimento web</b>, que foi dirigida pelo professor Bruno Prado.
+    </p>
+    </div>
+    <h3>Agradecimentos</h3>
+    <div>
+        <p class="linha">
+        Primeiramente eu agradeço a Deus por encontrar uma plataforma que vai me ajudar bastante, estudando e reforçando conceitos da programação.
+        </p>
+        <p class="linha">
+        Agradeço toda a equipe da plataforma DIO por oferecer a nós, alunos no ramo de TI, aulas gratuitas de programação para que pudéssemos ter recursos o suficiente para nos tornar bons profissionais no vasto mundo da profissão de TI.
+        </p>
+        <p class="linha">
+        Agradeço também aos professores que disponibilizam aulas maravilhosas e ricas de conhecimento sobre as tecnologias de programação que estudei, que ainda estou estudando e que irei estudar. Vocês são demais!!!
+        </p>
+        <p class="linha">
+        E, por fim, agradeço a minha mãe e minhas irmãs por me darem apoio para continuar a estudar e melhorar a cada dia.<b> Obrigado por tudo!!!</b>
+        </p>
+    </div>
+    <h3>Sobre as aulas</h3>
+    <div>
+        <p class="linha">
+        As aulas em si são muito boas, cheios de detalhes e exercícios para alunos exercitarem e obter mais conhecimentos que não foram abordadas nas aulas. Tem 8 tópicos de aulas no total, sendo elas:
+        </p>
+        <ol>
+        <li><b>O que é Internet</b></li>
+        <li><b>Como funciona a Internet</b></li>
+        <li><b>TCP/IP, portas, roteadores, switches e modems</b></li>
+        <li><b>Celular, internet e outros dispositivos</b></li>
+        <li><b>Browser, sites, aplicativos e webserver</b></li>
+        <li><b>O que são Stacks</b></li>
+        <li><b>LPs e termos</b></li>
+        <li><b>Aula prática</b></li>
+        </ol>
+    </div>
+    <h3>Conclusão</h3>
+    <div>
+        <p>
+        Esta aula me ensinou muitos assuntos que antes não sabia. Sendo duas das demais aulas que eu mais gostei foram: O que são Stacks: aborda o seu significado e o que as definições de Front-end, Back-end e Full-Stack que trabalham, respectivamente, no visual, nos bancos de dados e servidores e em todas as tecnologias para o desenvolvimento de aplicativo/software; e Aula prática: Mostrando o passo a passo para instalar XAMPP e usar o PHP para criar um site.
+        </p>
+    </div>
+    </div>
+</body>
+</html>
